0bada6f697bde9b01fe950ca711a7b7d9b11d2ee
Notme112 <84899746+Notme112@users.noreply.github.com>
2021-12-06 10:01:25 +0100

    Create placeForScripts.user.js

---
+++ cristmas/placeForScripts.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Platz für Scripte!
+// @version      1.0.0
+// @description  Macht Platz für Scripte! ;-)
+// @author       NiZi112
+// @match        https://rettungssimulator.online/
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $("body").html("<h1>Platz für Scripte...</h1><h2>...aber keiner mehr zum Spielen! ;-)");
+})();
