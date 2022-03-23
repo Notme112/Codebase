663364752e3ff460ed2afe5b78e6b00dbc890cf7
Niklas <not_my_email@email.de>
2022-03-23 14:33:02 +0100

    added install.user.js

--- install.user.js
+++ install.user.js
@@ -1,1 +1,16 @@
-
+// ==UserScript==
+// @name         ReSi-Codebase
+// @version      1.5.2
+// @description  Erweitert viele Funktionen und fügt neue hinzu. Das alle kostenlos in einem Browsergamne!
+// @author       NiZi112
+// @match        https://rettungssimulator.online/*
+// @include      www.rettungssimulator.online
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @updateURL    https://github.com/Notme112/Codebase/raw/main/install.user.js
+// @downloadURL  https://github.com/Notme112/Codebase/raw/main/install.user.js
+// @grant        none
+// ==/UserScript==
+/* global $ sounds openFrame socket systemMessage */
+(() => {
+    document.head.appendChild((document.createElement('script')).src = 'https://raw.githubusercontent.com/Notme112/Codebase/main/src/codebase.min.js')
+})
