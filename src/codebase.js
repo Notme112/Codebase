c747d0f1110d6ae80b98fbf62e4e358ee4ff154e
Niklas <not_my_email@email.de>
2022-05-26 21:13:36 +0200

    add choises

--- src/codebase.js
+++ src/codebase.js
@@ -970,6 +970,48 @@ outline: none;
             allSite: false,
             settings: []
         },
+        {
+            name: "searchInMissionOverview",
+            description: "Fügt eine Suche zur Einsatzübersicht hinzu.",
+            settingsTarget: "searchInMissionOverview",
+            version: "1.0.0",
+            author: "NiZi112",
+            target: "searchInMissionOverviewCheck",
+            func: async (s) => {
+                if (!location.pathname.includes("/missionOverview")) return;
+                $('.detail-subtitle:first').append(`<div class="input-container right"><input class="input-round input-inline nochange" type="text" value="" style="padding-left:10px;" id="input_search" placeholder="Suche..." autocomplete="off"></div>`)
+                $('.detail-header').after(`<h4 class='label label-info searchNoResult hidden'>Die Suche lieferte keine Ergebnisse! Bitte probiere es mit einem anderen Suchwort!</h4>`)
+                $('head').append(`<style>.searchHidden { display: none !important };</style>`)
+                let query = 'table tbody tr';
+                function search() {
+                    var searchWord = document.querySelector('#input_search').value.toLowerCase();
+                    if (searchWord == '') {
+                        document.querySelectorAll(query).forEach((el) => {
+                            el.classList.remove('searchHidden');
+                            document.querySelector('.searchNoResult').classList.add('hidden');
+                        });
+                        return;
+                    }
+                    let elems = document.querySelectorAll(query);
+                    for (var j = 0; j < document.querySelectorAll(query).length; j++) {
+                        if (elems[j].querySelectorAll('td')[1].innerText.toLowerCase().includes(searchWord) || elems[j].querySelectorAll('td')[1].innerText.toLowerCase().includes(searchWord)) {
+                            elems[j].classList.remove('searchHidden');
+                            document.querySelector('.searchNoResult').classList.add('hidden');
+                        } else {
+                            elems[j].classList.add('searchHidden')
+                        }
+                    }
+                    if (elems.length == document.querySelectorAll('.searchHidden td').length) {
+                        document.querySelector('.searchNoResult').classList.remove('hidden');
+                    }
+                };
+                $('#input_search').on('input change keyup', search);
+            },
+            keywords: ["Suche", "Serach", "Mission", "Overview", "Übersicht", "Einsatz","Mission", "Einsatzübersicht"],
+            hasSettings: false,
+            allSite: true,
+            settings: []
+        }
     ];
     //function build default storage object from modules
     function buildDefaultStorage() {
