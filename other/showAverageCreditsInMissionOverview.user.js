d9e2849dcb8f06db1cd3cb710a87c19f06fa5853
Niklas <not_my_email@email.de>
2022-06-11 19:48:29 +0200

    added new script

---
+++ other/showAverageCreditsInMissionOverview.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         AverageMoneyInMissionOverview
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  shows the average amount of money per mission in total
+// @author       NiZi112
+// @match        https://rettungssimulator.online/missionOverview
+// @icon         http://rettungssimulator.online/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function getAverageMissionCredits(){
+        let list = document.querySelectorAll('table > tbody > tr > td:nth-child(3) > a');
+        let sum = 0;
+        list.forEach((el) => {
+            sum += parseInt(el.innerHTML.replaceAll('.', ''))
+        })
+        return sum / list.length
+    }
+    $('.detail-subtitle').append(`<span class="label label-primary">Durchschnittlicher Verdienst pro Einsatz: ${getAverageMissionCredits()}</span>`)
+})();
