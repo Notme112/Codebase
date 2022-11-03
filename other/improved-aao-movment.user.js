b0e12f6b5427b0e9d5e198c2b0de1946d51e0586
Notme112 <84899746+Notme112@users.noreply.github.com>
2022-11-03 17:36:21 +0100

    Update improved-aao-movment.user.js

--- other/improved-aao-movment.user.js
+++ other/improved-aao-movment.user.js
@@ -16,14 +16,14 @@
         .prepend('<span class="move-more-forward"><i class="fa fa-chevrons-left" style="padding-right:2px;"></i></span>')
         .append('<span class="move-more-backward"><i class="fa fa-chevrons-right"  style="padding-left:2px;"></i></span>')
         .css('width', '100px')
-    $('.move-more-forward').on('click', (t) => {
+    $(document).on('click', '.move-more-forward', (t) => {
         for(let i = 0; i < MOVING_STEPS; i++){
-            $(t.target).next().click();
+            $(t.target).parent('span').next().click();
         }
     })
-    $('.move-more-backward').on('click', (t) => {
+    $(document).on('click', '.move-more-backward', (t) => {
         for(let i = 0; i < MOVING_STEPS; i++){
-            $(t.target).prev().click();
+            $(t.target).parent('span').prev().click();
         }
     });
 })();
